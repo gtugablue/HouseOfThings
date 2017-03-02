acb644a60f2019cd27fb74f3614e09317d2aa698
FilipaRamos <fimamora@hotmail.com>
2017-03-02 13:46:36 +0000

    added image

--- web/js/main.js
+++ web/js/main.js
@@ -4,7 +4,11 @@
 function refresh() {
   $.get("http://127.0.0.1:8080/lights/1/", function(data) {
       $("#light-status").html("ON");
+      $("#off").hide();
+      $("#on").show();
     }).fail(function(data) {
       $("#light-status").html("OFF");
+      $("#on").hide();
+      $("#off").show();
     });
 }
