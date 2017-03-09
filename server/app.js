c0b4ddcd2352f107bddaec5f6f4d95f5f16e7d3b
Gustavo Silva <silva95gustavo@gmail.com>
2017-03-09 11:05:48 +0000

    Cleanup.

--- server/app.js
+++ server/app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 
-var index = require('./routes/index');
 var lights = require('./routes/lights');
 
 var app = express();
@@ -20,10 +19,6 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -32,7 +27,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
 app.use('/lights', lights);
 
 // catch 404 and forward to error handler
@@ -50,7 +44,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.end();
 });
 
 module.exports = app;
